fa0aa71e3a1a3055a7c6f276221e31bb29ff6514
Alexandre Fernandes <alexandrefilho.06@hotmail.com>
2024-09-03 14:51:08 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -42,43 +42,50 @@ fi
 # Function to add the function to the shell config file
 add_function() {
     SHELL_CONFIG_FILE="$1"
+    SHELL_TYPE="$2"
 
-    # Remove existing alias if it exists
-    if grep -q "alias cpt=" "$SHELL_CONFIG_FILE"; then
-        echo -e "${YELLOW}${INFO} Removing existing cpt alias from $SHELL_CONFIG_FILE...${RESET}"
-        sed -i '/alias cpt=/d' "$SHELL_CONFIG_FILE"
-    fi
+    # Remove existing function or alias if it exists
+    sed -i '/cpt()/,/}/d' "$SHELL_CONFIG_FILE"
+    sed -i '/alias cpt=/d' "$SHELL_CONFIG_FILE"
 
-    # Add the new function
-    if ! grep -q "cpt()" "$SHELL_CONFIG_FILE"; then
-        echo -e "${YELLOW}${INFO} Adding function to $SHELL_CONFIG_FILE...${RESET}"
+    echo -e "${YELLOW}${INFO} Adding function to $SHELL_CONFIG_FILE...${RESET}"
+    
+    if [ "$SHELL_TYPE" = "fish" ]; then
+        echo '' >> "$SHELL_CONFIG_FILE"
+        echo 'function cpt' >> "$SHELL_CONFIG_FILE"
+        echo '    if test "$argv[1]" = "--upgrade"' >> "$SHELL_CONFIG_FILE"
+        echo "        git -C \"$CPT_DIR\" pull \"$REPO_URL\"" >> "$SHELL_CONFIG_FILE"
+        echo '    else' >> "$SHELL_CONFIG_FILE"
+        echo "        bash \"$SCRIPT_FILE\" \$argv" >> "$SHELL_CONFIG_FILE"
+        echo '    end' >> "$SHELL_CONFIG_FILE"
+        echo 'end' >> "$SHELL_CONFIG_FILE"
+    else
         echo '' >> "$SHELL_CONFIG_FILE"
         echo 'cpt() {' >> "$SHELL_CONFIG_FILE"
-        echo '    if [ "$1" == "--upgrade" ]; then' >> "$SHELL_CONFIG_FILE"
+        echo '    if [[ "$1" = "--upgrade" ]]; then' >> "$SHELL_CONFIG_FILE"
         echo "        git -C \"$CPT_DIR\" pull \"$REPO_URL\"" >> "$SHELL_CONFIG_FILE"
         echo '    else' >> "$SHELL_CONFIG_FILE"
         echo "        bash \"$SCRIPT_FILE\" \"\$@\"" >> "$SHELL_CONFIG_FILE"
         echo '    fi' >> "$SHELL_CONFIG_FILE"
         echo '}' >> "$SHELL_CONFIG_FILE"
-        echo -e "${GREEN}${CHECK_MARK} Function added to $SHELL_CONFIG_FILE.${RESET}"
-    else
-        echo -e "${RED}${CROSS_MARK} Function already exists in $SHELL_CONFIG_FILE.${RESET}"
     fi
+
+    echo -e "${GREEN}${CHECK_MARK} Function added to $SHELL_CONFIG_FILE.${RESET}"
 }
 
 # Add function to bash, zsh, and fish if they exist
 if [ -f "$HOME_DIR/.bashrc" ]; then
-    add_function "$HOME_DIR/.bashrc"
+    add_function "$HOME_DIR/.bashrc" "bash"
 fi
 
 if [ -f "$HOME_DIR/.zshrc" ]; then
-    add_function "$HOME_DIR/.zshrc"
+    add_function "$HOME_DIR/.zshrc" "zsh"
 fi
 
 if [ -d "$HOME_DIR/.config/fish" ]; then
     FISH_CONFIG_FILE="$HOME_DIR/.config/fish/config.fish"
-    add_function "$FISH_CONFIG_FILE"
+    add_function "$FISH_CONFIG_FILE" "fish"
 fi
 
 # Inform user to reload shell configurations
-echo -e "${YELLOW}${ALERT} Please restart your shell to apply the new function.${RESET}"
+echo -e "${YELLOW}${ALERT} Please restart your shell or source your configuration file to apply the new function.${RESET}"
