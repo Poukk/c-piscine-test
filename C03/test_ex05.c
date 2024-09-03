d96a015401f6d13ecfb7c5ea939c995e8b33379e
Alexandre Fernandes <alexandrefilho.06@hotmail.com>
2024-09-03 13:59:46 -0300

    Update test_ex05.c

--- C03/test_ex05.c
+++ C03/test_ex05.c
@@ -2,6 +2,30 @@
 
 unsigned int ft_strlcat(char *dest, char *src, unsigned int size);
 
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+	
+	dest_len = 0;
+	src_len = 0;
+	while (dest[dest_len] && dest_len < size)
+		dest_len++;
+	while (src[src_len])
+		src_len++;
+	if (size <= dest_len)
+		return (size + src_len);
+	i = 0;
+	while (src[i] && dest_len + i + 1 < size)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
+}
+
 void cpt_test_exercise() {
     print_exercise("05", "ft_strlcat");
     char dest5[20] = "Hello";
